a2bdbb7e2c2454d37b3560500bf04f13ea9e4d99
tomsza26 <tomsza27@gmail.com>
2020-03-02 12:01:22 +0100

    nowe metarmofozy

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,10 +10,10 @@ function AboutMe() {
 					<div id="aboutMeTrener">TRENER PERSONALNY</div>
 					<img id="aboutPic1" src={require('../images/aboutMeCircle.png')} alt="about" />
 					<div id="aboutMeContent">
-						Cześć! Jak zauważyliście jestem pasjonatem sportu. Od 9 lat poznaje tajniki różnych treningów,
-						sposobów odżywiania się i podejścia do tych tematów. A od 3 lat jestem zawodowo w branży jako
-						trener. Przez ten czas współpracowałem z dużą ilością osób i większość z nich osiągnęła swoje
-						cele! Powstanie tej strony pozwala mi znacznie zwiększyć doświadczenie. Wiąże się z tym
+						Cześć! Jak mogliście zauważyć, jestem pasjonatem sportu. Od ponad 9 lat pogłębiam wiedzę o
+						treningach, żywieniu oraz podejściu mentalnym w tych dziedzinach. Od trzech lat pracuje w branży
+						jako trener. Przez ten czas współpracowałem z dużą ilością osób i większość z nich osiągnęła
+						swoje cele! Powstanie tej strony pozwala mi znacznie zwiększyć doświadczenie. Wiąże się z tym
 						dopracowanie moich metod aby były jeszcze bardziej skuteczne. Od zawsze sport w moim życiu grał
 						ważną rolę. Jeśli jesteś ciekawy jakie dyscypliny uprawiałem i jak zaczynałem pracę jako trener,
 						wystarczy, że klikniesz poniższy przycisk.
