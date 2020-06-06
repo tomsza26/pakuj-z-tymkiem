67e020757f0c006d641e4252502760b3dce05618
tomsza26 <tomsza27@gmail.com>
2020-06-06 13:50:43 +0200

    metamorfoza nr 3

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,40 +1,52 @@
 import React from 'react';
-import { NavHashLink as Link } from 'react-router-hash-link';
+import { NavHashLink as Link } from 'react-router-hash-link'; //gatsby link
 
 function AboutMe() {
-	return (
-		<article id="aboutMe">
-			<div id="aboutMeContainer">
-				<div id="aboutMeTextContainer" data-aos="fade-right" data-aos-duration="3000" data-aos-once="true">
-					<div id="aboutMeName">Tymek Nowak</div>
-					<div id="aboutMeTrener">TRENER PERSONALNY</div>
-					<img id="aboutPic1" src={require('../images/aboutMeCircle.png')} alt="about" />
-					<div id="aboutMeContent">
-						Cześć! Jak mogliście zauważyć, jestem pasjonatem sportu. Od ponad 9 lat pogłębiam wiedzę o
-						treningach, żywieniu oraz podejściu mentalnym w tych dziedzinach. Od trzech lat pracuje w branży
-						jako trener. Przez ten czas współpracowałem z dużą ilością osób i większość z nich osiągnęła
-						swoje cele! Powstanie tej strony pozwala mi znacznie zwiększyć doświadczenie. Wiąże się z tym
-						dopracowanie moich metod aby były jeszcze bardziej skuteczne. Od zawsze sport w moim życiu grał
-						ważną rolę. Jeśli jesteś ciekawy jakie dyscypliny uprawiałem i jak zaczynałem pracę jako trener,
-						wystarczy, że klikniesz poniższy przycisk.
-					</div>
-					<Link to="/oMnie#aboutContainer">
-						<div href="" id="aboutMeButton">
-							CZYTAJ WIĘCEJ
-						</div>
-					</Link>
-				</div>
-				<img
-					id="aboutPic2"
-					src={require('../images/aboutMeCircle.png')}
-					alt="about"
-					data-aos="fade-left"
-					data-aos-duration="3000"
-					data-aos-once="true"
-				/>
-			</div>
-		</article>
-	);
+  return (
+    <article id="aboutMe">
+      <div id="aboutMeContainer">
+        <div
+          id="aboutMeTextContainer"
+          data-aos="fade-right"
+          data-aos-duration="3000"
+          data-aos-once="true"
+        >
+          <div id="aboutMeName">Tymek Nowak</div>
+          <div id="aboutMeTrener">TRENER PERSONALNY</div>
+          <img
+            id="aboutPic1"
+            src={require('../images/aboutMeCircle.png')}
+            alt="about"
+          />
+          <div id="aboutMeContent">
+            Cześć! Jak mogliście zauważyć, jestem pasjonatem sportu. Od ponad 9
+            lat pogłębiam wiedzę o treningach, żywieniu oraz podejściu mentalnym
+            w tych dziedzinach. Od trzech lat pracuje w branży jako trener.
+            Przez ten czas współpracowałem z dużą ilością osób i większość z
+            nich osiągnęła swoje cele! Powstanie tej strony pozwala mi znacznie
+            zwiększyć doświadczenie. Wiąże się z tym dopracowanie moich metod
+            aby były jeszcze bardziej skuteczne. Od zawsze sport w moim życiu
+            grał ważną rolę. Jeśli jesteś ciekawy jakie dyscypliny uprawiałem i
+            jak zaczynałem pracę jako trener, wystarczy, że klikniesz poniższy
+            przycisk.
+          </div>
+          <Link to="/oMnie#aboutContainer">
+            <div href="" id="aboutMeButton">
+              CZYTAJ WIĘCEJ
+            </div>
+          </Link>
+        </div>
+        <img
+          id="aboutPic2"
+          src={require('../images/aboutMeCircle.png')}
+          alt="about"
+          data-aos="fade-left"
+          data-aos-duration="3000"
+          data-aos-once="true"
+        />
+      </div>
+    </article>
+  );
 }
 
 export default AboutMe;
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,122 +1,134 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendarAlt, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
-import { NavHashLink as Link } from 'react-router-hash-link';
+import {
+  faCalendarAlt,
+  faExternalLinkAlt,
+} from '@fortawesome/free-solid-svg-icons';
+import { NavHashLink as Link } from 'react-router-hash-link'; //gatsby
 import articles from '../content/articles.json';
 
 class Blog extends React.Component {
-	shorten = (str, maxLen, separator = ' ') => {
-		if (str.length <= maxLen) return str;
-		return str.substr(0, str.lastIndexOf(separator, maxLen)) + '...';
-	};
+  shorten = (str, maxLen, separator = ' ') => {
+    if (str.length <= maxLen) return str;
+    return str.substr(0, str.lastIndexOf(separator, maxLen)) + '...';
+  };
 
-	render() {
-		let blog = articles
-			.sort(function(a, b) {
-				return b.id - a.id;
-			})
-			.slice(0, 3)
-			.map((data, index) => {
-				let cate = data.kategoria
-					.replace(/\s+/g, '_')
-					.replace('ą', 'a')
-					.replace('ć', 'c')
-					.replace('ó', 'o')
-					.replace(/[^\w\s]/gi, '')
-					.toLowerCase();
+  render() {
+    let blog = articles
+      .sort(function (a, b) {
+        return b.id - a.id;
+      })
+      .slice(0, 3)
+      .map((data, index) => {
+        let cate = data.kategoria
+          .replace(/\s+/g, '_')
+          .replace('ą', 'a')
+          .replace('ć', 'c')
+          .replace('ó', 'o')
+          .replace(/[^\w\s]/gi, '')
+          .toLowerCase();
 
-				return (
-					<Link
-						to={`/blog/${cate}/${data.id}#navContainer`}
-						key={index}
-						className="blogArs"
-						id={`hel${index}`}
-					>
-						<div className="blogArticle">
-							<div className="blogArticleCategory">{data.kategoria}</div>
-							<div className="blogArticleHeader">{data.nazwa}</div>
-							<div className="blogArticleContent">{this.shorten(data.tekst, 300)}</div>
-							<div className="blogArticleDate">
-								<FontAwesomeIcon icon={faCalendarAlt} /> {data.data}
-								<FontAwesomeIcon icon={faExternalLinkAlt} />
-							</div>
-						</div>
-					</Link>
-				);
-			});
+        return (
+          <Link
+            to={`/blog/${cate}/${data.id}#navContainer`}
+            key={index}
+            className="blogArs"
+            id={`hel${index}`}
+          >
+            <div className="blogArticle">
+              <div className="blogArticleCategory">{data.kategoria}</div>
+              <div className="blogArticleHeader">{data.nazwa}</div>
+              <div className="blogArticleContent">
+                {this.shorten(data.tekst, 300)}
+              </div>
+              <div className="blogArticleDate">
+                <FontAwesomeIcon icon={faCalendarAlt} /> {data.data}
+                <FontAwesomeIcon icon={faExternalLinkAlt} />
+              </div>
+            </div>
+          </Link>
+        );
+      });
 
-		let blog2 = articles
-			.sort(function(a, b) {
-				return b.id - a.id;
-			})
-			.slice(1, 3)
-			.map((data, index) => {
-				let cate = data.kategoria
-					.replace(/\s+/g, '_')
-					.replace('ą', 'a')
-					.replace('ć', 'c')
-					.replace('ó', 'o')
-					.replace(/[^\w\s]/gi, '')
-					.toLowerCase();
+    let blog2 = articles
+      .sort(function (a, b) {
+        return b.id - a.id;
+      })
+      .slice(1, 3)
+      .map((data, index) => {
+        let cate = data.kategoria
+          .replace(/\s+/g, '_')
+          .replace('ą', 'a')
+          .replace('ć', 'c')
+          .replace('ó', 'o')
+          .replace(/[^\w\s]/gi, '')
+          .toLowerCase();
 
-				return (
-					<Link
-						to={`/blog/${cate}/${data.id}#navContainer`}
-						key={index}
-						className="blogArs"
-						id={`helRes${index}`}
-					>
-						<div className="blogArticle">
-							<div className="blogArticleCategory">{data.kategoria}</div>
-							<div className="blogArticleHeader">{data.nazwa}</div>
-							<div className="blogArticleContent">{this.shorten(data.tekst, 300)}</div>
-							<div className="blogArticleDate">
-								<FontAwesomeIcon icon={faCalendarAlt} /> {data.data}
-								<FontAwesomeIcon icon={faExternalLinkAlt} />
-							</div>
-						</div>
-					</Link>
-				);
-			});
+        return (
+          <Link
+            to={`/blog/${cate}/${data.id}#navContainer`}
+            key={index}
+            className="blogArs"
+            id={`helRes${index}`}
+          >
+            <div className="blogArticle">
+              <div className="blogArticleCategory">{data.kategoria}</div>
+              <div className="blogArticleHeader">{data.nazwa}</div>
+              <div className="blogArticleContent">
+                {this.shorten(data.tekst, 300)}
+              </div>
+              <div className="blogArticleDate">
+                <FontAwesomeIcon icon={faCalendarAlt} /> {data.data}
+                <FontAwesomeIcon icon={faExternalLinkAlt} />
+              </div>
+            </div>
+          </Link>
+        );
+      });
 
-		return (
-			<section id="blog">
-				<header id="blogHeader">BLOG</header>
-				<div id="headerContainer">
-					<div className="blogContentHeader1">najnowsze artykuły</div>
-					<div className="blogContentHeader">szybki start</div>
-				</div>
+    return (
+      <section id="blog">
+        <header id="blogHeader">BLOG</header>
+        <div id="headerContainer">
+          <div className="blogContentHeader1">najnowsze artykuły</div>
+          <div className="blogContentHeader">szybki start</div>
+        </div>
 
-				<div id="blogContainer">
-					<div id="blogButtonsContainer" data-aos="fade-right" data-aos-duration="2500" data-aos-once="true">
-						<Link to="/blog/dieta#navContainer">
-							<div className="blogButton">dieta</div>
-						</Link>
-						<Link to="/blog/trening#navContainer">
-							<div className="blogButton">trening</div>
-						</Link>
-						<Link to="/blog/jak_zaczac#navContainer">
-							<div className="blogButton" id="blogStart">
-								jak zacząć?
-							</div>
-						</Link>
-						<Link to="/blog/ciekawostki#navContainer">
-							<div className="blogButton">ciekawostki</div>
-						</Link>
-						<Link to="/blog/rozwoj_osobisty#navContainer">
-							<div className="blogButton">rozwój osobisty</div>
-						</Link>
-						<Link to="/blog/plany_treningowe#navContainer">
-							<div className="blogButton">plany treningowe</div>
-						</Link>
-					</div>
-					<div id="blogContentHeader2">najnowsze artykuły</div>
-					<div id="blogGrid">{blog}</div>
-					<div id="blogRes">{blog2}</div>
-				</div>
-			</section>
-		);
-	}
+        <div id="blogContainer">
+          <div
+            id="blogButtonsContainer"
+            data-aos="fade-right"
+            data-aos-duration="2500"
+            data-aos-once="true"
+          >
+            <Link to="/blog/dieta#navContainer">
+              <div className="blogButton">dieta</div>
+            </Link>
+            <Link to="/blog/trening#navContainer">
+              <div className="blogButton">trening</div>
+            </Link>
+            <Link to="/blog/jak_zaczac#navContainer">
+              <div className="blogButton" id="blogStart">
+                jak zacząć?
+              </div>
+            </Link>
+            <Link to="/blog/ciekawostki#navContainer">
+              <div className="blogButton">ciekawostki</div>
+            </Link>
+            <Link to="/blog/rozwoj_osobisty#navContainer">
+              <div className="blogButton">rozwój osobisty</div>
+            </Link>
+            <Link to="/blog/plany_treningowe#navContainer">
+              <div className="blogButton">plany treningowe</div>
+            </Link>
+          </div>
+          <div id="blogContentHeader2">najnowsze artykuły</div>
+          <div id="blogGrid">{blog}</div>
+          <div id="blogRes">{blog2}</div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Blog;
